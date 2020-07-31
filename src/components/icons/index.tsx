731dc357ab5b224d6f7b77fb3cd0599bb9669f81
John Letey <johnletey@gmail.com>
2020-07-31 13:26:32 +0100

    feat: Add social links

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+export { default as GitHub } from "./github";
+export { default as Instagram } from "./instagram";
+export { default as LinkedIn } from "./linkedin";
+export { default as Mail } from "./mail";
+export { default as Twitter } from "./twitter";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,26 @@
+import { Mail, GitHub, Twitter, LinkedIn, Instagram } from "@components/icons";
+
 const Home = () => {
-  return <h1>Coming Soon</h1>;
+  return (
+    <>
+      <h1>Coming Soon.</h1>
+      <a href="mailto:johnletey@gmail.com">
+        <Mail />
+      </a>
+      <a href="https://github.com/jletey">
+        <GitHub />
+      </a>
+      <a href="https://twitter.com/johnletey">
+        <Twitter />
+      </a>
+      <a href="https://www.linkedin.com/in/johnletey">
+        <LinkedIn />
+      </a>
+      <a href="https://instagram.com/johnletey">
+        <Instagram />
+      </a>
+    </>
+  );
 };
 
 export default Home;
