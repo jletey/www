7f8c278da53cf6de1391eeef314bc92d6004f3c3
John Letey <johnletey@gmail.com>
2020-07-31 16:33:43 +0100

    feat: Add SEO and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,39 @@
 import "../styles.css";
+import { DefaultSeo } from "next-seo";
+
+const title = "John Letey";
+const description = "Software Engineer and Designer";
+const SEO = {
+  title,
+  description,
+  canonical: "https://johnletey.vercel.app",
+  openGraph: {
+    type: "website",
+    locale: "en_GB",
+    url: "https://johnletey.vercel.app",
+    title,
+    description,
+    images: [
+      {
+        url: "/og.png",
+        alt: title,
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    handle: "@johnletey",
+    site: "@johnletey",
+    cardType: "summary_large_image",
+  },
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  );
 }
